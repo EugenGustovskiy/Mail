4425488498c9f0e22a071ee36455ef61a5c32356
Eugen Gustovskiy <eu.hustouski@gmail.com>
2023-07-11 23:51:01 +0200

    Task_6

---
+++ Mail/LogivPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mail
+{
+    public class LogivPage : BasePage
+    {
+        IWebElement _fieldLogin;
+        
+        const string FIELD_LOGIN_XPATH = "//input[1]";
+        const string FURTHER_XPATH = "//button[contains(@class, 'qIypjc TrZEUc lw1w4b')]";
+        
+        public LogivPage(IWebDriver driver) : base(driver)
+        {
+            InitializeLogin();
+        }
+
+        private void InitializeLogin()
+        {
+            _fieldLogin = FindMailElement(FIELD_LOGIN_XPATH);
+        }
+
+        public void EnterLogin(string login)
+        {
+            _fieldLogin.SendKeys(login);
+            var buttonFurther = FindMailElement(FURTHER_XPATH);
+            ClickElement(buttonFurther);
+        }
+    }
+}
---
+++ Mail/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+
+namespace Mail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            IndexPage gmailPage = new IndexPage(driver);
+            LogivPage loginPage = gmailPage.OpenLoginPage();
+            loginPage.EnterLogin("proj.m.first@gmail.com");
+            driver.Close();
+        }
+    }
+}
